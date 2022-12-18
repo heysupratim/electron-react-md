415338e4e4f646a43cda52db3a87a1751995190b
Supratim <sup270792@gmail.com>
2022-12-18 01:10:54 -0800

    Remove shim

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import './shim'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
--- src/shim.js
+++
@@ -1,6 +0,0 @@
-// import process from "process";
-
-// if (typeof global === 'undefine' || typeof global.process === 'undefined'){
-//     window.global = window
-//     window.process = process
-// }
